0b9e3532dee03ca189de39f750c0919c130d264a
Boris Burkov <b.burkov@gmail.com>
2016-03-19 22:17:42 +0000

    refactor lib/recall.py into lib/recall module

--- cli/recall.py
+++ cli/recall.py
@@ -1,6 +1,7 @@
 import click
 
-import lib.recall
+import lib.recall.next_action
+import lib.recall.project
 
 @click.group()
 @click.pass_context
@@ -24,9 +25,9 @@ def project(ctx, name):
     If a name is given, Eidetic will show detailed information about that project
     '''
     if not name:
-        click.echo(lib.recall.recall_names())
+        click.echo(lib.recall.project.recall_names())
     else:
-        for p_md in lib.recall.recall_project(name):
+        for p_md in lib.recall.project.recall_project(name):
             click.echo(p_md)
 
 @recall.command()
@@ -41,7 +42,7 @@ def next_action(ctx, name):
     If a name is given, return the next action for that project.
     '''
     if not name:
-        for p, next_action in lib.recall.recall_next_actions():
+        for p, next_action in lib.recall.next_action.recall_next_actions():
             click.echo('{}: {}'.format(p, next_action))
     else:
-        click.echo(lib.recall.recall_next_action(name))
+        click.echo(lib.recall.next_action.recall_next_action(name))
--- lib/recall/project.py
+++ lib/recall/project.py
@@ -1,6 +1,6 @@
 import rethinkdb as r
 
-import db
+import lib.db as db
 
 def format_project_markdown(project):
     lines = []
@@ -40,30 +40,3 @@ def _recall_names(conn):
 def recall_names():
     conn = db.get_conn()
     return _recall_names(conn)
-
-def _get_next_action(actions):
-    next_actions = [a for a in actions if a['next']]
-    if not next_actions:
-        return None
-    return next_actions[0]['description']
-
-def _recall_next_action(name, conn):
-    c = db.get_table().filter({'name': name})['actions'].run(conn)
-    project_actions = [actions for actions in c]
-    if not project_actions:
-        return None
-    actions = project_actions[0]
-    return _get_next_action(actions)
-
-def _recall_next_actions(conn):
-    c = db.get_table().run(conn)
-    for project in c:
-        yield project['name'], _get_next_action(project['actions'])
-
-def recall_next_actions():
-    conn = db.get_conn()
-    return _recall_next_actions(conn)
-
-def recall_next_action(name):
-    conn = db.get_conn()
-    return _recall_next_action(name, conn)
